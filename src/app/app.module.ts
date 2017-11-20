91fe8960af8ae9843a47d4cb3cb499246c63bca5
rasko <rasko@ebi.ac.uk>
2017-11-20 16:29:30 +0000

    First working paginator and flex trials.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {NgModule} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 import {CdkTableModule} from '@angular/cdk/table';
 import {
@@ -82,6 +83,7 @@ const appRoutes: Routes = [
   imports: [
       BrowserModule,
       BrowserAnimationsModule,
+      FlexLayoutModule,      
       FormsModule,
       HttpModule,
       HttpClientModule,
--- src/app/spreadsheet-submission/spreadsheet-submission.component.ts
+++ src/app/spreadsheet-submission/spreadsheet-submission.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { Observable } from 'rxjs/Rx';
-import { MatTableDataSource } from '@angular/material';
+import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { MatPaginator, MatTableDataSource } from '@angular/material';
 
 import { SubmissionFormatSelectorComponent } from '../submission-format-selector/submission-format-selector.component';
 import { SubmissionSpreadsheetSelectorComponent } from '../submission-spreadsheet-selector/submission-spreadsheet-selector.component';
 
 import { WebinRestService } from '../webin-rest.service';
 
+import {Observable} from 'rxjs/Observable';
+ import {Subject} from 'rxjs/Subject';
 
-export interface webinErrorTableRow {
+export interface WebinError {
   error: string;
 }
 
@@ -25,37 +26,62 @@ export interface webinErrorTableRow {
 export class SpreadsheetSubmissionComponent implements OnInit {
 
   webinErrorTableColumns = ['error'];
-  webinErrorDataSource: MatTableDataSource<WebinErrorTableRow>;
+  webinErrorDataSource: WebinErrorDataSource;
+  result;
+  @ViewChild(MatPaginator) webinErrorPaginator: MatPaginator;
 
   submissionType: string;
   submissionFormat: string;
   submissionSpreadsheet: string;
   spreadsheetFile: File;
   action: string = 'add';
+  errorDisplay: string = 'table';
+
+  constructor(
+    private webinRestService: WebinRestService) {
+  }
+
+  ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+  }
+
+  getNumberOfErrors() {
+    console.log(this.result);
+    if (this.result == null || this.result.errors == null) {
+      return 0;
+    }
+    return this.result.errors.length;
+  }
 
   consumeSubmissionTypeChange(submissionType: string) {
     this.submissionType = submissionType;
     this.submissionFormat = undefined;
-    this.webinErrorDataSource = undefined;
+    this.webinErrorDataSource = null;
+    this.result = null;
     // console.info('Received changed submission type: ' + submissionType);
   }
 
   consumeSubmissionFormatChange(submissionFormat: string) {
     this.submissionFormat = submissionFormat;
-    this.webinErrorDataSource = undefined;
+    this.webinErrorDataSource = null;
+    this.result = null;
     // console.info('Received changed submission format: ' + submissionFormat);
   }
 
   consumeSubmissionSpreadsheetChange(submissionSpreadsheet: string) {
     this.submissionSpreadsheet = submissionSpreadsheet;
-    this.webinErrorDataSource = undefined;
+    this.webinErrorDataSource = null;
+    this.result = null;
     // console.info('Received changed submission spreadsheet: ' + submissionSpreadsheet);
   }
 
   // https://stackoverflow.com/questions/35399617/angular-2-file-upload-from-input-type-file
   onChangeSpreadsheetFile(files) {
     this.spreadsheetFile = files[0];
-    this.webinErrorDataSource = undefined;
+    this.webinErrorDataSource = null;
+    this.result = null;
     //console.info("Spreadsheet file: " + this.spreadsheetFile);
   }
 
@@ -86,9 +112,11 @@ export class SpreadsheetSubmissionComponent implements OnInit {
           // Success
           data => {
               // HttpResponse when using {observe: 'response'}
-              let result = this.webinRestService.parseResult(data);
-              console.log('** Webin spreadsheet submission succeeded **', result);
-              this.webinErrorDataSource = new MatTableDataSource<WebinErrorTableRow>(result.errors);
+              this.result = this.webinRestService.parseResult(data);
+              console.log('** Webin spreadsheet submission succeeded **', this.result);
+
+              this.webinErrorDataSource = new MatTableDataSource<WebinError>(this.result.errors);
+              this.webinErrorDataSource.paginator = this.webinErrorPaginator;
           },
           // Errors
           (err: HttpErrorResponse) => {
@@ -102,13 +130,5 @@ export class SpreadsheetSubmissionComponent implements OnInit {
             }
         });
       }
-  }
-
-
-  constructor(
-    private webinRestService: WebinRestService) {
-  }
-
-  ngOnInit() {
   }
 }
