826ce07128f999ed29f5a7db2a0db4565725394d
josue <josue@ebi.ac.uk>
2017-12-13 12:49:48 +0000

    Add spring boot support

---
+++ src/main/java/uk/ac/ebi/ena/ang/web/SubAngApplication.java
@@ -0,0 +1,17 @@
+package uk.ac.ebi.ena.ang.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.system.ApplicationPidFileWriter;
+
+@SpringBootApplication
+public class SubAngApplication {
+
+	private static String PID_FILE = "sub-ang-release.pid";
+
+	public static void main(String[] args) {
+		SpringApplication springApplication = new SpringApplication(SubAngApplication.class);
+		springApplication.addListeners(new ApplicationPidFileWriter(PID_FILE));
+		springApplication.run(args);
+	}
+}
