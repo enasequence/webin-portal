c12eb0b228540af80450f9002f12d6ec60d7c85e
rasko <rasko@ebi.ac.uk>
2018-09-24 17:11:18 +0100

    Added all required fields to the form.

--- frontend/src/app/update-request/update-request.component.ts
+++ frontend/src/app/update-request/update-request.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateRequestComponent implements OnInit {
 
+  id: string;
+  status: string;
+  authority: string;
+  affects: string;
+  reason: string;
+  currentValue: string;
+  newValue: string;
+
   constructor() { }
 
   ngOnInit() {
   }
-
 }
