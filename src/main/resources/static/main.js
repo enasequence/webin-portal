e7c41943ca41828ad3272ae7005b72d3c96a1dde
Rasko Leinonen <rasko@ebi.ac.uk>
2019-10-01 14:14:22 +0100

    Updated support link.

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -3939,12 +3939,10 @@ __webpack_require__.r(__webpack_exports__);
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 var environment = {
     production: false,
-    title: 'Webin submissions portal (DEFAULT)',
+    title: 'Webin submissions portal (DEV)',
     webinServiceUrl: 'https://wwwdev.ebi.ac.uk/ena/submit/drop-box/submit/',
-    webinUpdateRequestUrl: 'https://wwwdev.ebi.ac.uk/ena/submit/drop-box/email/update-request/',
     webinAuthenticationServiceUrl: 'https://www.ebi.ac.uk/ena/auth/login',
     webinAuthenticationTokenUrl: 'https://www.ebi.ac.uk/ena/auth/token',
-    // webinReportServiceUrl: 'http://RASKO-W7D.windows.ebi.ac.uk:8221/ena/submit/report',
     webinReportServiceUrl: 'https://wwwdev.ebi.ac.uk/ena/submit/report',
     webinXmlReportServiceUrl: 'https://wwwdev.ebi.ac.uk/ena/submit/drop-box',
     webinGdprServiceUrl: 'TODO'
