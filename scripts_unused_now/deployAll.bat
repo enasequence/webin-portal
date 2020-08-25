06a04f3676f7c2ad8f9958a9bbe301ec2d118a1b
dgupta <dgupta@ebi.ac.uk>
2020-08-25 15:54:27 +0100

    Gitlab CI added

--- scripts_unused_now/deployAll.bat
+++
@@ -1,8 +0,0 @@
-:PROMPT
-    SET /P AREYOUSURE=Are you sure spring boot jars will be deployed in production (Y/[N])?
-    IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
-    call gradlew -Penv=dev clean deploy
-    call gradlew -Penv=test clean deploy
-    call gradlew -Penv=prod clean deploy
-    call gradlew clean
-    :END
