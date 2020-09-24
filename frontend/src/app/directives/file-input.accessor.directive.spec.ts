6dba2cd375404506bba075761c373e990a216833
rajkumar <rajkumar.devraj@gmail.com>
2020-09-24 07:57:06 +0100

    Adding Sample Registration code

--- frontend/src/app/directives/file-input.accessor.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FileInput.AccessorDirective } from './file-input.accessor.directive';
-
-describe('FileInput.AccessorDirective', () => {
-  it('should create an instance', () => {
-    const directive = new FileInput.AccessorDirective();
-    expect(directive).toBeTruthy();
-  });
-});
