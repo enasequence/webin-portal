88056f5f00fef1bfa86ace81667ba2aecfed1341
rajkumar <rajkumar.devraj@gmail.com>
2020-11-17 07:10:46 +0000

    Side nav changes

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,17 +4,16 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: true,
-  title: 'Webin submissions portal',
-  webinServiceUrl: 'https://www.ebi.ac.uk/ena/submit/drop-box/submit/',
-  webinUpdateRequestUrl: 'https://www.ebi.ac.uk/ena/submit/drop-box/email/update-request/',
-  webinAuthenticationServiceUrl: 'https://www.ebi.ac.uk/ena/submit/webin/auth/login',
-  webinAuthenticationTokenUrl: 'https://www.ebi.ac.uk/ena/submit/webin/auth/token',
-  webinReportServiceUrl: 'https://www.ebi.ac.uk/ena/submit/report',
-  webinXmlReportServiceUrl: 'https://www.ebi.ac.uk/ena/submit/drop-box',
-  webinAdminServiceUrl: 'https://www.ebi.ac.uk/ena/submit/webin/auth/admin',
+  production: false,
+  title: 'Webin submissions portal (DEV)',
+  webinServiceUrl: 'http://ves-ebi-5b:8110/ena/submit/drop-box/submit/',
+  webinUpdateRequestUrl: 'http://ves-ebi-5b:8110/ena/submit/drop-box/email/update-request/',
+  webinAuthenticationServiceUrl: 'https://wwwdev.ebi.ac.uk/ena/dev/submit/webin/auth/login',
+  webinAuthenticationTokenUrl: 'https://wwwdev.ebi.ac.uk/ena/dev/submit/webin/auth/token',
+  webinReportServiceUrl: 'http://ves-ebi-5b:8221/ena/submit/report',
+  webinXmlReportServiceUrl: 'http://ves-ebi-5b:8110/ena/submit/drop-box',
+  webinAdminServiceUrl: 'https://wwwdev.ebi.ac.uk/ena/dev/submit/webin/auth/admin',
   webinGdprServiceUrl: 'TODO',
   pupMedUrl: 'https://www.ebi.ac.uk/europepmc/webservices/rest/search',
-  
   
 };
