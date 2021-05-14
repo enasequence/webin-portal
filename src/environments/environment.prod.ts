9e51bf185ef95788ba652ab6299be67a086e7e5c
Rajkumar-D <rajkumar.devraj@gmail.com>
2021-05-14 17:28:44 +0530

    Update environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 
 export const environment = {
   production: false,
-  title: 'Webin submissions portal (TEST)',
+  title: 'Webin submissions portal',
   webinRestUrl: 'https://www.ebi.ac.uk/ena/submit/drop-box',
   webinAuthUrl: 'https://www.ebi.ac.uk/ena/submit/webin/auth',
   webinReportServiceUrl: 'https://www.ebi.ac.uk/ena/submit/report',
