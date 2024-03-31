ae4ea1e44c5af22673d55be375c8959b42b24444
dipayan1985 <dgupta@ebi.ac.uk>
2024-03-31 12:30:07 +0100

    Ng upgrade step 1: to 11

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@
  * specific language governing permissions and limitations under the License.
  */
 
+import '@angular/compiler';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
