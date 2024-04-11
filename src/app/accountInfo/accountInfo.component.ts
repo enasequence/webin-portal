337a2a2487b0bdf3d32d1800a738e69103e5d64b
dipayan1985 <dgupta@ebi.ac.uk>
2024-04-11 10:21:19 +0100

    Post upgrade: sanction accounts

--- src/app/accountInfo/accountInfo.component.ts
+++ src/app/accountInfo/accountInfo.component.ts
@@ -9,16 +9,20 @@
  * specific language governing permissions and limitations under the License.
  */
 
-import { Component, ViewEncapsulation, ViewChild } from "@angular/core";
-import { MatDialog } from "@angular/material/dialog";
-import { ContactDialogModalComponent } from "../contact-dialog-modal/contact-dialog-modal.component";
-import { MatTableDataSource } from "@angular/material/table";
-import { UtilService } from "../util/Util-services";
-import { Router, ActivatedRoute } from "@angular/router";
-import { WebinAuthenticationService } from "../webin-authentication.service";
-import { Compiler } from "@angular/core";
-import { ResetPasswordRequestDialogComponent } from "../reset-password-request-dialog/reset-password-request-dialog.component";
-import { NonSubmissionResultDialogComponent } from "../non-submission-result-dialog/non-submission-result-dialog.component";
+import {Component, ViewEncapsulation, ViewChild} from "@angular/core";
+import {MatDialog} from "@angular/material/dialog";
+import {ContactDialogModalComponent} from "../contact-dialog-modal/contact-dialog-modal.component";
+import {MatTableDataSource} from "@angular/material/table";
+import {UtilService} from "../util/Util-services";
+import {Router, ActivatedRoute} from "@angular/router";
+import {WebinAuthenticationService} from "../webin-authentication.service";
+import {Compiler} from "@angular/core";
+import {
+  ResetPasswordRequestDialogComponent
+} from "../reset-password-request-dialog/reset-password-request-dialog.component";
+import {
+  NonSubmissionResultDialogComponent
+} from "../non-submission-result-dialog/non-submission-result-dialog.component";
 
 @Component({
   selector: "app-main",
@@ -49,6 +53,7 @@ export class AccountInfoComponent {
   metagenomicsAnalysis = false;
   noEffectCheckbox = false;
   countryErr = false;
+  sanctionedCountryError = false;
 
   /* Used for storing added emails, this will be used for validation */
   emails = [];
@@ -83,14 +88,15 @@ export class AccountInfoComponent {
     });
   }
 
-  ngOnDestroy() { }
+  ngOnDestroy() {
+  }
 
   openDialog(action, obj): void {
     const dialogRef = this.dialog.open(ContactDialogModalComponent, {
       width: "500px",
       backdropClass: "custom-dialog-backdrop-class",
       panelClass: "custom-dialog-panel-class",
-      data: { action: action, emailList: this.emails, contactObj: obj },
+      data: {action: action, emailList: this.emails, contactObj: obj},
     });
 
     dialogRef.afterClosed().subscribe((result) => {
@@ -290,14 +296,17 @@ export class AccountInfoComponent {
       width: "400px",
       backdropClass: "custom-dialog-backdrop-class",
       panelClass: "custom-dialog-panel-class",
-      data: { resetObj: obj },
+      data: {resetObj: obj},
     });
   }
 
   selectCountryOption(event: any): void {
-    this.countryErr = false;
-    if (event.target.value != "" && this.countries.indexOf(event.target.value) === -1) {
-      this.countryErr = true;
-    }
+    const selectedCountry = event.target.value;
+
+    // Check if the selected country is not an empty string and is not in the countries array
+    this.countryErr = selectedCountry !== '' && this.countries.indexOf(selectedCountry) === -1;
+
+    // Check if the selected country is Russia or Belarus
+    this.sanctionedCountryError = selectedCountry === 'Russia' || selectedCountry === 'Belarus';
   }
 }
